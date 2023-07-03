b5b596de2f28df5becdca1827f75347f33fc941d
Lisyona <129197066+Lisyona@users.noreply.github.com>
2023-07-03 21:57:06 +0300

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-access_token = 'https://oauth.vk.com/blank.html#access_token=vk1.a.wjefyk_law92tQthurDXFI2KVoA2eRpOu68jV6EJOI4jzBLfIs-IpR7JvLXPBqxbmquilTEcLERdfQuW4kAq8OCZftgfQ3_CYmucfWuL2pdcWQgdBNxZ1zG9IbiYebjZOMxKQMTlE9KAtXzsZTPx0ir8fCqxNn7WTa1IhmRyUgDM-yGIoFzAfkKn_kizopv2&expires_in=86400&user_id=15206350'
-community_token = 5osBKv8CLGiNWkrLJqX2
+access_token = 
+community_token = 
 DSN = 'postgresql://postgres:Lisyona@localhost:5432/books'
 engine = sqlalchemy.create_engine(DSN)
 Session = sessionmaker(bind = engine)
